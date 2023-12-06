b9cc486dc2a7dc98e7d907a71bb28872b303810d
Gabriel <gabrivacaro@gmail.com>
2023-12-06 01:18:41 +0100

    Ejercicio 1C no funcional

--- PI3/src/ejercicios/Ejercicio1.java
+++ PI3/src/ejercicios/Ejercicio1.java
@@ -6,20 +6,21 @@ import java.util.function.Predicate;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.vertexcover.GreedyVCImpl;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 import datos.Relacion;
 import datos.Usuario;
 import us.lsi.colors.GraphColors;
 import us.lsi.colors.GraphColors.Color;
-import us.lsi.grafos.datos.Carretera;
-import us.lsi.grafos.datos.Ciudad;
+
 import us.lsi.graphs.views.SubGraphView;
 
 public class Ejercicio1 {
 	
+	
 	// Crea vista del grafo
-	public static void ejercicio1A(String file, Graph<Usuario,Relacion> g, Predicate<Usuario> pv, 
+	public static void ejercicio1A(String file, SimpleDirectedWeightedGraph<Usuario,Relacion> g, Predicate<Usuario> pv, 
 			Predicate<Relacion> pa, String nombreVista) {
 		
 		Graph<Usuario, Relacion> vista = SubGraphView.of(g, pv, pa);
@@ -30,6 +31,13 @@ public class Ejercicio1 {
 				e->GraphColors.colorIf(Color.red, vista.containsEdge(e)));
 		
 		System.out.println(file + nombreVista + ".gv generado en " + "resultados/ejercicio1");
+	}
+	
+	private static Color asignaColor(Usuario u, List<Set<Usuario>> ls, 
+			ConnectivityInspector<Usuario, Relacion> alg) {
+		Color[] vc = Color.values();
+		Set<Usuario> s = alg.connectedSetOf(u);
+		return vc[ls.indexOf(s)];
 	}
 	
 	public static void ejercicio1B(SimpleDirectedWeightedGraph<Usuario, Relacion> g, String file) {	
@@ -45,14 +53,16 @@ public class Ejercicio1 {
 		
 		System.out.println(file + "B.gv generado en " + "resultados/ejercicio1");
 	}
-	
-	
-	private static Color asignaColor(Usuario u, List<Set<Usuario>> ls, 
-			ConnectivityInspector<Usuario, Relacion> alg) {
-		Color[] vc = Color.values();
-		Set<Usuario> s = alg.connectedSetOf(u);
-		return vc[ls.indexOf(s)];
+	public static void ejercicio1C(SimpleDirectedWeightedGraph<Usuario, Relacion> g, String file) {
+		
+		var alg = new GreedyVCImpl<>(g);
+		Set<Usuario> vc = alg.getVertexCover();		
+		
+		GraphColors.toDot(g,"resultados/ejercicio1/" + file + "B.gv",
+				x->x.nombre(), x->x.interaccion().toString(),
+				v->GraphColors.colorIf(Color.red, vc.contains(v)),
+				e->GraphColors.color(Color.red));
+		
+		System.out.println(file + "C.gv generado en " + "resultados/ejercicio1");
 	}
-	
-	
 }
--- PI3/src/tests/TestEjercicio1.java
+++ PI3/src/tests/TestEjercicio1.java
@@ -34,35 +34,26 @@ public class TestEjercicio1 {
 							Graphs2::simpleDirectedWeightedGraph,
 							Relacion::interaccion); //creador del grafo
 		
-		/*
-		 * //Para mostrar el grafo original
-		GraphColors.toDot(g,"resultados/ejercicio1/" + file + ".gv",
-				v->v.nombre(), //que etiqueta mostrar en vertices y aristas
-				e->e.interaccion().toString(),
-				v->GraphColors.color(Color.black), //color o estilo de vertices y aristas
-				e->GraphColors.color(Color.black));
-		*/
-		
 		System.out.println("\nArchivo " + file + ".txt \n" + "Datos de entrada: " + g);
 		
 // A: usuarios que siga a mas de 3 personas y media de las aristas dirigidas a los que sigue mayor que 2.5	
 		
 		Predicate<Usuario> pv1 = u -> {
 		    Set<Relacion> edges = g.outgoingEdgesOf(u);
-		    // Filtrar las aristas por el índice de interacción
 		    List<Double> interacciones = edges.stream()
-		            .map(r -> r.interaccion())  // Suponiendo que hay un método Relacion.interaccion() que devuelve el índice de interacción
+		            .map(r -> r.interaccion())  
 		            .collect(Collectors.toList());
 		    Double total = interacciones.stream().reduce(0.0, Double::sum);
-		    // Verificar la condición
 		    return edges.size() > 3 && total/interacciones.size() > 2.5;
 		};
 		Predicate<Relacion> pa1= null;
 		
 		Ejercicio1.ejercicio1A(file, g,pv1,pa1,"A");
-		
 		// B: Cantidad de componentes conexas y pintarlas cada una con distinto color
 		Ejercicio1.ejercicio1B(g, file);
+		// C: Aplicar algoritmo de cobertura de vertices minima
+	//	Ejercicio1.ejercicio1C(g, file);
+		
 	}
 		
 		
