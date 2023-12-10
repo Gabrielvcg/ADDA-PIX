5676407d85cc1db90e9ed03303b41165a24b06aa
Gabriel <gabrivacaro@gmail.com>
2023-12-10 19:05:37 +0100

    Ejercicio 3b finalizado

--- PI3/src/ejercicios/Ejercicio3.java
+++ PI3/src/ejercicios/Ejercicio3.java
@@ -1,7 +1,12 @@
 package ejercicios;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.jgrapht.graph.SimpleDirectedGraph;
 import org.jgrapht.traverse.TopologicalOrderIterator;
@@ -34,4 +39,47 @@ public class Ejercicio3 {
 	System.out.println("Orden de tareas: "+tareas);
 	System.out.println(file + nombreVista + ".gv generado en " + "resultados/ejercicio3");
 	}
+	
+
+	public static void ejercicio3B(String file, SimpleDirectedGraph<Tarea, Precedencia> g, Tarea tareaDada, String nombreVista) {
+	    // Obtener tareas necesarias para completar la tarea dada
+	    Set<Tarea> tareasNecesarias = obtenerTareasNecesarias(g, tareaDada);
+
+	    GraphColors.toDot(g, "resultados/ejercicio3/" + file + nombreVista + ".gv",
+	            x -> x.nombre(), x -> "Relacion-" + x.id().toString(),
+	            v -> GraphColors.color(tareaDada.equals(v) ? Color.red :(tareasNecesarias.contains(v) ? Color.blue : Color.black)),
+	            e -> GraphColors.color(tareasNecesarias.contains(g.getEdgeSource(e)) &&
+	                                   tareasNecesarias.contains(g.getEdgeTarget(e)) ? Color.blue : Color.black));
+
+	    List<String> tareas = new ArrayList<>();
+	    tareasNecesarias.forEach(tarea -> tareas.add(0,tarea.nombre()));
+	    
+	    tareas.remove(tareaDada.nombre());
+	    
+	    System.out.println("\nEJERCICIO 3B ======================================================");
+	    System.out.println("Tareas necesarias para completar " + tareaDada.nombre() + ": " + tareas);
+	    System.out.println(file + nombreVista + ".gv generado en " + "resultados/ejercicio3");
+	}
+	private static Set<Tarea> obtenerTareasNecesarias(SimpleDirectedGraph<Tarea, Precedencia> g, Tarea tareaDada) {
+	    Set<Tarea> tareasNecesarias = new HashSet<>();
+	    Queue<Tarea> cola = new LinkedList<>();
+	    cola.offer(tareaDada);
+
+	    while (!cola.isEmpty()) {
+	        Tarea tareaActual = cola.poll();
+	        tareasNecesarias.add(tareaActual);
+
+	     // Obtener predecesores de la tarea actual
+	        Set<Tarea> predecesores = new HashSet<>();
+	        for (Precedencia edge : g.incomingEdgesOf(tareaActual)) {
+	            predecesores.add(g.getEdgeSource(edge));
+	        }
+
+	        // Agregar predecesores a la cola si no han sido visitados
+	        predecesores.stream().filter(predecesor -> !tareasNecesarias.contains(predecesor)).forEach(cola::offer);
+	    }
+
+	    return tareasNecesarias;
+	}
+	
 }
--- PI3/src/tests/TestEjercicio3.java
+++ PI3/src/tests/TestEjercicio3.java
@@ -12,15 +12,15 @@ import us.lsi.graphs.GraphsReader;
 public class TestEjercicio3 {
 
 	public static void main(String[] args) {
-		testsEjercicio3("ejercicio3_1");
+		testsEjercicio3F1("ejercicio3_1");
 		System.out.println("=========================================================================================");
-		testsEjercicio3("ejercicio3_2");
+		testsEjercicio3F2("ejercicio3_2");
 		System.out.println("===========================================================================================");
-		testsEjercicio3("ejercicio3_3");
+		testsEjercicio3F3("ejercicio3_3");
 	
 		}
 	
-	public static void testsEjercicio3(String file) {
+	public static void testsEjercicio3F1(String file) {
 		SimpleDirectedGraph<Tarea, Precedencia> g = GraphsReader
 					.newGraph("ficheros/" + file + ".txt", //fichero de datos
 							Tarea::ofFormat, //factoria para construir los vertices
@@ -30,5 +30,34 @@ public class TestEjercicio3 {
 		System.out.println("\nArchivo " + file + ".txt \n" + "Datos de entrada: " + g);
 		
 		Ejercicio3.ejercicio3A(file, g,"A");
+		Ejercicio3.ejercicio3B(file, g,new Tarea("Tarea5"),"B");		
+
 	}
+	public static void testsEjercicio3F2(String file) {
+		SimpleDirectedGraph<Tarea, Precedencia> g = GraphsReader
+					.newGraph("ficheros/" + file + ".txt", //fichero de datos
+							Tarea::ofFormat, //factoria para construir los vertices
+							Precedencia::ofFormat, //factoria para crear las aristas
+							Graphs2::simpleDirectedGraph);
+		
+		System.out.println("\nArchivo " + file + ".txt \n" + "Datos de entrada: " + g);
+		
+		Ejercicio3.ejercicio3A(file, g,"A");
+		Ejercicio3.ejercicio3B(file, g,new Tarea("Tarea8"),"B");		
+
+	}
+	public static void testsEjercicio3F3(String file) {
+		SimpleDirectedGraph<Tarea, Precedencia> g = GraphsReader
+					.newGraph("ficheros/" + file + ".txt", //fichero de datos
+							Tarea::ofFormat, //factoria para construir los vertices
+							Precedencia::ofFormat, //factoria para crear las aristas
+							Graphs2::simpleDirectedGraph);
+		
+		System.out.println("\nArchivo " + file + ".txt \n" + "Datos de entrada: " + g);
+		
+		Ejercicio3.ejercicio3A(file, g,"A");
+		Ejercicio3.ejercicio3B(file, g,new Tarea("Tarea8"),"B");		
+
+	}
+	
 }
